5b9711b7ea398bad95d853ba40ed994502b50838
Alexandre Osada <alexandre.osada@gmail.com>
2021-03-14 21:11:47 -0300

    Inclusão da verificação do python3 para carregar os snippets

--- nvim/.config/nvim/lua/osada/ultisnips.lua
+++ nvim/.config/nvim/lua/osada/ultisnips.lua
@@ -1,4 +1,6 @@
-vim.g.completion_enable_snippet = 'UltiSnips'
-vim.g.UltiSnipsExpandTrigger="<Tab>"
-vim.g.UltiSnipsJumpForwardTrigger="<Tab>"
-vim.g.UltiSnipsJumpBackwardTrigger="<S-Tab>"
+if vim.fn.has('python3') then
+    vim.g.completion_enable_snippet = 'UltiSnips'
+    vim.g.UltiSnipsExpandTrigger="<Tab>"
+    vim.g.UltiSnipsJumpForwardTrigger="<Tab>"
+    vim.g.UltiSnipsJumpBackwardTrigger="<S-Tab>"
+end
