9eae1177d87ce7e318e44cee196183d80ef13fb6
Alexandre Osada <alexandre.osada@gmail.com>
2021-03-14 21:11:21 -0300

    Alteração nos atalhos do telescope

--- nvim/.config/nvim/lua/osada/telescope.lua
+++ nvim/.config/nvim/lua/osada/telescope.lua
@@ -2,7 +2,7 @@ local actions = require('telescope.actions')
 require('telescope').setup {
     defaults = {
         file_sorter = require('telescope.sorters').get_fzy_sorter,
-        prompt_prefix = ' >',
+        prompt_prefix = ' 🔍',
         color_devicons = true,
 
         file_previewer   = require('telescope.previewers').vim_buffer_cat.new,
@@ -26,10 +26,10 @@ require('telescope').setup {
 
 require('telescope').load_extension('fzy_native')
 
-vim.api.nvim_set_keymap('n', '<leader>ff', ':Telescope find_files<CR>', { noremap = true, silent = true })
+vim.api.nvim_set_keymap('n', '<leader>pf', ':Telescope find_files<CR>', { noremap = true, silent = true })
 vim.api.nvim_set_keymap('n', '<C-p>', ':Telescope git_files<CR>', { noremap = true, silent = true })
-vim.api.nvim_set_keymap('n', '<leader>fb', ':Telescope buffers<CR>', { noremap = true, silent = true })
-vim.api.nvim_set_keymap('n', '<leader>ft', ':Telescope help_tags<CR>', { noremap = true, silent = true })
+vim.api.nvim_set_keymap('n', '<leader>pb', ':Telescope buffers<CR>', { noremap = true, silent = true })
+vim.api.nvim_set_keymap('n', '<leader>pt', ':Telescope help_tags<CR>', { noremap = true, silent = true })
 vim.api.nvim_set_keymap('n', '<leader>gb', ':Telescope git_branches<CR>', { noremap = true, silent = true })
 
 --nnoremap <leader>fp :lua require('telescope.builtin').grep_string({ search = vim.fn.input("Grep For > ")})<CR>
