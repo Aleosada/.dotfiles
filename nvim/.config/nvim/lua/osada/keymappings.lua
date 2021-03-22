eef2245979bf604dc01f145ea809f7956bc02c32
Alexandre Osada <alexandre.osada@gmail.com>
2021-03-22 08:02:20 -0300

    Melhora na navegação do autocomplete com C-j e C-k

--- nvim/.config/nvim/lua/osada/keymappings.lua
+++ nvim/.config/nvim/lua/osada/keymappings.lua
@@ -4,5 +4,9 @@ vim.api.nvim_set_keymap('n', 'C-z', '<nop>', { noremap = true, silent = true })
 vim.api.nvim_set_keymap('v', '<', '<gv', { noremap = true, silent = true })
 vim.api.nvim_set_keymap('v', '>', '>gv', { noremap = true, silent = true })
 
+-- Better nav for omnicomplete
+vim.cmd('inoremap <expr> <c-j> (\"\\<C-n>\")')
+vim.cmd('inoremap <expr> <c-k> (\"\\<C-p>\")')
+
 --Maximizer maps
 vim.api.nvim_set_keymap('n', '<leader>ww', ':MaximizerToggle<CR>', { noremap = true, silent = true })
