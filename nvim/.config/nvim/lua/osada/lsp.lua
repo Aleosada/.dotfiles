2a03faa9e86c8cd6131b092789c396ee2db4b95f
Alexandre Osada <alexandre.osada@gmail.com>
2021-04-25 15:02:56 -0300

    Inclusão do lsp para deno

--- nvim/.config/nvim/lua/osada/lsp.lua
+++ nvim/.config/nvim/lua/osada/lsp.lua
@@ -1,7 +1,7 @@
 local on_attach = require'completion'.on_attach
 local nvim_lsp = require('lspconfig')
 
-local servers = { "pyright", "jsonls", "tsserver", "vimls", "yamlls", "dockerls", "angularls" }
+local servers = { "pyright", "jsonls", "tsserver", "vimls", "yamlls", "dockerls", "angularls", "denols" }
 for _, lsp in ipairs(servers) do
   nvim_lsp[lsp].setup { on_attach = on_attach }
 end
