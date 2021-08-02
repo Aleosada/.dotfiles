9bc63d260f5ceb921c8f6930c1fae4cadb971522
Alexandre Osada <alexandre.osada@gmail.com>
2021-08-02 17:54:26 -0300

    Adicionando alguns keymappings para o vim:
    1. Copiar até o fim da linha "Y"
    2. Undo breakpoints
    3. Mover textos para cima e para baixo

--- nvim/.config/nvim/lua/osada/keymappings.lua
+++ nvim/.config/nvim/lua/osada/keymappings.lua
@@ -16,5 +16,22 @@ vim.api.nvim_set_keymap('n', '<leader>wo', ':VimwikiIndex<CR>', { noremap = true
 
 -- Copy to clipboard maps
 vim.api.nvim_set_keymap('n', '<leader>y', '"+y', { noremap = true, silent = true })
+vim.api.nvim_set_keymap('n', '<leader>Y', 'gg"+yG', { noremap = true, silent = true })
 vim.api.nvim_set_keymap('v', '<leader>y', '"+y', { noremap = true, silent = true })
-vim.api.nvim_set_keymap('v', '<leader>Y', 'gg"+yG', { noremap = true, silent = true })
+
+-- Copy until the end of the line
+vim.api.nvim_set_keymap('n', 'Y', 'y$', { noremap = true, silent = true })
+
+-- Move text
+vim.api.nvim_set_keymap('v', 'K', ":m '<-2<CR>gv=gv", { noremap = true, silent = true })
+vim.api.nvim_set_keymap('v', 'J', ":m '>+1<CR>gv=gv", { noremap = true, silent = true })
+vim.api.nvim_set_keymap('n', '<leader>k', ':m .-2<CR>==', { noremap = true, silent = true })
+vim.api.nvim_set_keymap('n', '<leader>j', ':m .+1<CR>==', { noremap = true, silent = true })
+vim.api.nvim_set_keymap('i', '<C-k>', ':<esc>:m .-2<CR>==', { noremap = true, silent = true })
+vim.api.nvim_set_keymap('i', '<C-j>', ':<esc>:m .+1<CR>==', { noremap = true, silent = true })
+
+-- Undo breakpoints
+vim.api.nvim_set_keymap('i', ',', ',<c-g>u', { noremap = true, silent = true })
+vim.api.nvim_set_keymap('i', ';', ';<c-g>u', { noremap = true, silent = true })
+vim.api.nvim_set_keymap('i', '!', '!<c-g>u', { noremap = true, silent = true })
+vim.api.nvim_set_keymap('i', '?', '?<c-g>u', { noremap = true, silent = true })
