0da787bd4a2f046ab404d56599127c5bb083774a
Alexandre Osada <alexandre.osada.dev@gmail.com>
2024-04-09 17:41:10 -0300

    feat: alteração do modelo de uso do chatgpt.nvim para o gpt-4

--- nvim/.config/nvim/lua/custom/chatgpt.lua
+++ nvim/.config/nvim/lua/custom/chatgpt.lua
@@ -5,10 +5,15 @@ return {
 		require("chatgpt").setup({
 			-- The API key for the OpenAI GPT
 			api_key_cmd = "pass openai.apikey",
+			openai_params = {
+				model = "gpt-4",
+			},
+			openai_edit_params = {
+				model = "gpt-4",
+			},
 		})
 
 		vim.keymap.set("n", "<leader>gg", "<cmd>ChatGPT<CR>", { noremap = true, silent = true })
-		-- vim.keymap.set("n", "<leader>ga", "<cmd>ChatGPTActAs<CR>", { noremap = true, silent = true })
 		vim.keymap.set("n", "<leader>ge", "<cmd>ChatGPTEditWithInstructions<CR>", { noremap = true, silent = true })
 		vim.keymap.set("n", "<leader>gr", ":ChatGPTRun ", { noremap = true, silent = true })
 	end,
